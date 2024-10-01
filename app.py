b040524c43626acbbcb2eff2178a3b5ace6dbf04
Mario Torrez Dev <infoemergentes@gmail.com>
2024-10-01 17:54:58 -0400

    Primer commit

---
+++ app.py
@@ -0,0 +1,12 @@
+# Funciones que devuelven valores
+def suma(x,y):
+    return x + y
+
+def producto(x,y):
+    return x * y
+
+resultado = suma(5,7)
+print(resultado)
+
+resul = producto(6,8)
+print("El producto es: "+str(resul))
