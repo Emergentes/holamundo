00ac106387149064edf8e5fd467a2e6fe7c08cce
Mario Torrez Dev <infoemergentes@gmail.com>
2024-10-02 06:37:01 -0400

    Adicionando repo.txt

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-# Funciones que devuelven valores
-def suma(x,y):
-    return x + y
+# Funciones - Son bloques de codigo que realiza una tarea específica y que son reutilizables
 
-def producto(x,y):
-    return x * y
+# Funcion sin parámetros ni devolución de valor
+def saludar():
+    print("Hola, bienvenidos al curso de Python")
+    
 
-resultado = suma(5,7)
-print(resultado)
+# Funcion con parámetros
+def saludo(nombre):
+    print("Hola "+nombre+" bienvenido a clases")
+    
+# Funcion que devuelve valores
+def suma(a, b):
+    return a + b
 
-resul = producto(6,8)
-print("El producto es: "+str(resul))
+# Establecer valores por defecto para los parametros de una funcion
+def bienvenida(nombre="Estudiante"):
+    print("Bienvenido ",nombre)
+    
+# Funcion con argumentos variables
+def sumador(*args):
+    return sum(args)
+
+ 
+# Llamada a la funcion
+print(sumador(1,2,3,4,5))
+print(sumador(4,5,6))
